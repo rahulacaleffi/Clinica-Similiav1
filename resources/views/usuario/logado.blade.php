4ab8b990d9f23e4bceead969fd8302862dc55f81
Danieltl21 <danieltrevisan532@gmail.com>
2018-04-15 21:23:08 -0300

    15-04-2018-Daniel-v03
    Ajustando página logado

--- resources/views/usuario/logado.blade.php
+++ resources/views/usuario/logado.blade.php
@@ -1,7 +1,7 @@
 <?php
 	if(Auth::check()){
-        echo "<h1>Você está logado</h1>"
+        echo "<h1>Você está logado</h1>";
     } else {
-    	echo "<h1>Você não está logado</h1>"
+    	echo "<h1>Você não está logado</h1>";
     }
 ?>
